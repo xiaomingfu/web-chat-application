44b313fdabaac7d1e2c04fd52a0dceb9ca7a2acd
Xiaoming Fu <xmingfu86@gmail.com>
2021-11-02 03:58:53 -0700

    Refactoring router

--- api/index.js
+++ api/index.js
@@ -2,11 +2,14 @@ const express = require("express");
 
 const config = require("./config/app");
 
+const router = require("./router");
+
 const app = express();
 
-app.get("/home", (req, res) => {
-  return res.send("Welcom Home");
-});
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(router);
 
 const port = config.appPort;
 
