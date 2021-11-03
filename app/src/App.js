a0b6d63215a31903a93cfd4c5203f22af66374c2
Xiaoming Fu <xmingfu86@gmail.com>
2021-11-03 00:26:47 -0700

    create initial components and add React Router

---
+++ app/src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+import Login from "./components/Auth/Login";
+import Register from "./components/Auth/Register";
+import Chat from "./components/Chat/Chat";
+
+import "./App.css";
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route exact path="/" component={Chat} />
+          <Route path="/login" component={Login} />
+          <Route path="/register" component={Register} />
+          <Route render={() => <h1>404 page not found</h1>} />
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
