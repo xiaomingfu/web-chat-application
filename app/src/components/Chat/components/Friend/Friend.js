aed7dd693c6bcab8affac47d3231bd6e7adb5867
Xiaoming Fu <xmingfu86@gmail.com>
2021-12-04 14:52:06 -0800

    add friend component

---
+++ app/src/components/Chat/components/Friend/Friend.js
@@ -0,0 +1,38 @@
+import React from "react";
+import "./Friend.scss";
+import { useSelector } from "react-redux";
+
+const Friend = ({ chat }) => {
+  const currentChat = useSelector((state) => state.chatReducer.currentChat);
+  const isChatOpened = () => {
+    return currentChat.id === chat.id ? "opened" : "";
+  };
+  const lastMessage = () => {
+    if (chat.Messages.length === 0) return "";
+    const message = chat.Messages[chat.Messages.length - 1];
+    return message.type === "image" ? "image uploaded" : message.message;
+  };
+  return (
+    <div className={`friend-list ${isChatOpened()}`}>
+      <div>
+        <img
+          width="40"
+          height="40"
+          src={chat.Users[0].avatar}
+          alt="User avatar"
+        />
+        <div className="friend-info">
+          <h4 className="m-0">
+            {chat.Users[0].firstName} {chat.Users[0].lastName}
+          </h4>
+          <h5 className="m-0">{lastMessage()}</h5>
+        </div>
+      </div>
+      <div class="friend-status">
+        <span className={`online-status`}></span>
+      </div>
+    </div>
+  );
+};
+
+export default Friend;
