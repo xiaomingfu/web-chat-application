d9d4498bda07b130fd4e003899d196735e13b4bd
Xiaoming Fu <xmingfu86@gmail.com>
2021-12-04 14:51:49 -0800

    add friendlist component

--- app/src/components/Chat/components/FriendList/FriendList.js
+++ app/src/components/Chat/components/FriendList/FriendList.js
@@ -1,9 +1,32 @@
 import "./FriendList.scss";
 
 import React from "react";
+import { useSelector } from "react-redux";
+import chatReducer from "../../../../store/reducers/chat";
+import Friend from "../Friend/Friend";
 
 const FriendList = () => {
-  return <h1>FriendList</h1>;
+  const chats = useSelector((state) => state.chatReducer.chats);
+
+  return (
+    <div id="friends">
+      <div id="title">
+        <h3 className="m-0">Friends</h3>
+        <button>ADD</button>
+      </div>
+      <hr />
+
+      <div id="friends-box">
+        {chats.length > 0 ? (
+          chats.map((chat) => {
+            return <Friend chat={chat} key={chat.id} />;
+          })
+        ) : (
+          <p>No friends added</p>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default FriendList;
