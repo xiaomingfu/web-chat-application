9ce255115f798f312323902f789fa6425ab3f162
Xiaoming Fu <xmingfu86@gmail.com>
2021-12-06 00:38:21 -0800

    add message style

---
+++ app/src/components/Chat/components/Message/Message.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "./Message.scss";
+
+const Message = ({ user, chat, message, index }) => {
+  const determinMargin = () => {
+    if (index + 1 === chat.Messages.length) return;
+    return message.fromUserId === chat.Messages[index + 1].fromUserId
+      ? "mb-5"
+      : "mb-10";
+  };
+  return (
+    <div
+      className={`message ${determinMargin()} ${
+        message.fromUserId === user.id ? "creator" : ""
+      }`}
+    >
+      <div
+        className={message.fromUserId === user.id ? "owner" : "other-person"}
+      >
+        {message.type === "text" ? (
+          <p className="m-0">{message.message}</p>
+        ) : (
+          <img src={message.message} alt="User upload" />
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Message;
--- app/src/components/Chat/components/MessageBox/MessageBox.js
+++ app/src/components/Chat/components/MessageBox/MessageBox.js
@@ -1,7 +1,24 @@
 import React from "react";
+import Message from "../Message/Message";
+import { useSelector } from "react-redux";
 
-const MessageBox = () => {
-  return <div>MessageBox</div>;
+const MessageBox = ({ chat }) => {
+  const user = useSelector((state) => state.authReducer.user);
+  return (
+    <div id="msg-box">
+      {chat.Messages.map((message, index) => {
+        return (
+          <Message
+            key={message.id}
+            chat={chat}
+            message={message}
+            index={index}
+            user={user}
+          />
+        );
+      })}
+    </div>
+  );
 };
 
 export default MessageBox;
